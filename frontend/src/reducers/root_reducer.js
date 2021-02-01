e747553796b65d3f7eb5d0fff7597c10336ec51b
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-02-01 01:28:37 -0500

    added tweet components

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
-import SessionErrorsReducer from './session_errors_reducer';
+import errors from './errors_reducer';
+import tweets from './tweets_reducer';
 
 const RootReducer = combineReducers({
-  session:session,
-  errors: SessionErrorsReducer
+  errors,
+  session,
+  tweets
 });
 
 export default RootReducer;
